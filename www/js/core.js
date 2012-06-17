074d827b431d78e109770e5655433a304d1b7b6a
Brent Anderson <brentj84062@gmail.com>
2012-06-17 17:59:49 -0600

    Worked out basically everything, but I need to get the PDF display to work properly. Commit going in before I start trying to integrate a ChildBrowser plugin.

---
+++ www/js/core.js
@@ -0,0 +1,37 @@
+var db;
+
+// If you want to prevent dragging, uncomment this section
+/*
+ function preventBehavior(e) 
+ { 
+ e.preventDefault(); 
+ };
+ document.addEventListener("touchmove", preventBehavior, false);
+ */
+
+/* If you are supporting your own protocol, the var invokeString will contain any arguments to the app launch.
+ see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+ for more details -jm */
+/*
+ function handleOpenURL(url)
+ {
+ // TODO: do something with the url passed in.
+ }
+ */
+
+function onBodyLoad()
+{		
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+/* When this function is called, Cordova has been initialized and is ready to roll */
+/* If you are supporting your own protocol, the var invokeString will contain any arguments to the app launch.
+ see http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+ for more details -jm */
+function onDeviceReady()
+{
+    db = window.openDatabase('hymns', '1.0', 'hymns', 3145728);
+    db.transaction(populateDB, errorCB, successCB);
+    db.transaction(queryDB, errorCB);
+}
+
