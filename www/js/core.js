1a4f4c665848c730c39c0b2631a752f882d1d49e
Brent Anderson <brentj84062@gmail.com>
2012-06-17 19:13:52 -0600

    Scrapping the UI tweaks for now. Going to deploy to device.

--- www/js/core.js
+++ www/js/core.js
@@ -53,12 +53,6 @@ function locChanged(loc) {
 function onOpenExternal() {
 }
 
-if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-    $(window).bind('orientationchange', function(event) {
-       if (window.orientation == 90 || window.orientation == -90 || window.orientation == 270) {
-           $('meta[name="viewport"]').attr('content', 'height=device-width,width=device-height,initial-scale=1.0,maximum-scale=1.0');
-       } else {
-           $('meta[name="viewport"]').attr('content', 'height=device-height,width=device-width,initial-scale=1.0,maximum-scale=1.0');
-       }
-   }).trigger('orientationchange');
-}
+$( window ).bind( "orientationchange", function( event ) {
+
+});
